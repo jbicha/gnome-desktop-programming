2d138d065bb5f5c332cfb952db266870771d362f
Christian Hergert <chris@dronelabs.com>
2012-09-25 04:13:45 -0700

    Update idea/student1.c

--- idea/student1.c
+++ idea/student1.c
@@ -6,7 +6,7 @@ main (int   argc,
 {
    int a, b;
 
-   printf("Please enter two numbers separted by a space to multiply:  ");
+   printf("Please enter two numbers separated by a space to multiply:  ");
    scanf("%d %d", &a, &b);
    printf("%d * %d = %d\n", a, b, a * b);
 
